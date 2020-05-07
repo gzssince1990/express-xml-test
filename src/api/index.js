690fa4ca12b239d60e08a6f8950f1dc8ebd2f9b1
Zhisong Ge <gzs253017369@gmail.com>
2020-05-07 01:28:38 -0400

    fix question2

--- src/api/index.js
+++ src/api/index.js
@@ -28,7 +28,10 @@ export default ({ config, db }) => {
 		const question2 = message_tag.ele('question', { id: 124, type: 'option', condition: 'post_submit' })
 		question2.ele('text', 'How is team WaterSky')
 		question2.ele('anwser', { id: 2 }, 'Awesome I guess?')
-		question2.ele('anwser', { id: 3, autofill: 1 }, 'Awesome!!!')
+		question2.ele('anwser', { id: 3, autofill: 3 }, 'Awesome!!!')
+		const question3 = message_tag.ele('question', { id: 125, type: 'manual', condition: 'post_submit' })
+		question3.ele('text', 'What is your favorite sport')
+		question3.ele('answer', { id: 4, autofill: 4 }, 'Basketball')
 		const xml = xml_tag.end({ pretty: true });
 		// console.log(xml)
 		res.set('Content-Type', 'text/xml');
